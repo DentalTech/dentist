233300d77eec095a037e0c5938b94ff8a6317c9e
Frank Mullen <frankmullen@gmail.com>
2017-02-13 14:20:41 +0000

    database url

--- settings/staging.py
+++ settings/staging.py
@@ -11,8 +11,9 @@ DATABASES = {
 }
 
 
+
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', '<your STRIPE_PUBLISHABLE key>')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', '<your STRIPE_SECRET key>')
 
-DATABASES['default'] = dj_database_url.config("<CLEARDB_DATABASE_URL>")
+DATABASES['default'] = dj_database_url.parse("mysql://bc5cb537891e4a:0e9e66b7@eu-cdbr-west-01.cleardb.com/heroku_27041bcc28780b7?reconnect=true")
