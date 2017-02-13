57598dbee38e0eb812dac9f37d9572dc3686791f
Frank Mullen <frankmullen@gmail.com>
2017-02-13 09:57:16 +0000

    added requirements.txt

---
+++ settings/dev.py
@@ -0,0 +1,20 @@
+from base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+INSTALLED_APPS.append('debug_toolbar')
+
+# Do I need to add this to middleware classes? See https://django-debug-toolbar.readthedocs.io/en/0.11.0/installation.htmlpp
+# 'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
