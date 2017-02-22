2f3790b02775563cbb7b76b632112a6a63a24c2f
Frank Mullen <frankmullen@gmail.com>
2017-02-22 12:44:53 +0000

    appointment slots working

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from appointments.models import Appointment
-from django.views.generic.edit import CreateView
+
 
 
 class AppointmentForm(forms.ModelForm):
--- appointments/models.py
+++ appointments/models.py
@@ -1,17 +1,52 @@
 from __future__ import unicode_literals
-from django.utils import timezone
 from accounts.models import User
-
-
 from django.db import models
+from datetime import date
+
 
 class Appointment(models.Model):
+    SLOT1 = '08:30'
+    SLOT2 = '09:00'
+    SLOT3 = '09:30'
+    SLOT4 = '10:00'
+    SLOT5 = '10:30'
+    SLOT6 = '11:00'
+    SLOT7 = '11:30'
+    SLOT8 = '12:00'
+    SLOT9 = '14:00'
+    SLOT10 = '14:30'
+    SLOT11 = '15:00'
+    SLOT12 = '15:30'
+    SLOT13 = '16:00'
+    SLOT14 = '16:30'
 
     patient_name = models.ForeignKey(User)
-    appointment_date = models.DateTimeField(default=timezone.now())
+    appointment_date = models.DateField(default=date.today)
+    appointment_time_choices = (
+        (SLOT1, '08:30'),
+        (SLOT2, '09:00'),
+        (SLOT3, '09:30'),
+        (SLOT4, '10:00'),
+        (SLOT5, '10:30'),
+        (SLOT6, '11:00'),
+        (SLOT7, '11:30'),
+        (SLOT8, '12:00'),
+        (SLOT9, '14:00'),
+        (SLOT10, '14:30'),
+        (SLOT11, '15:00'),
+        (SLOT12, '15:30'),
+        (SLOT13, '16:00'),
+        (SLOT14, '16:30'),
+    )
+    appointment_time = models.CharField(
+        max_length=5,
+        choices=appointment_time_choices,
+        default=SLOT1,
+    )
 
     def __unicode__(self):
-        return str(self.appointment_date) + " - " + str(self.patient_name)
+        return str(self.appointment_date) + " - " + str(self.appointment_time) + " - " + str(self.patient_name)
+
 
 
 
