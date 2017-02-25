2b59298672e62a6c432f489934247793a83c064b
Frank Mullen <frankmullen@gmail.com>
2017-02-25 12:53:04 +0000

    fixed stripe tags

--- settings/base.py
+++ settings/base.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, 'static'), )
+STATICFILES_DIRS = os.path.join(PROJECT_ROOT, 'static'),
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
